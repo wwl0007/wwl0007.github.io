fc5e765b5e99c7890a3f109222fdb4d453c14a94
Wesley Lowman <72044648+wwl0007@users.noreply.github.com>
2023-10-30 23:06:05 -0500

    Create EliotWeimarLocations.js

---
+++ EliotWeimarLocations.js
@@ -0,0 +1,278 @@
+var EliotWeimarLocations = {
+   "type": "FeatureCollection",
+   "features": [
+    {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -0.12775, 51.50735]
+    },
+    "properties": {
+    "Date":"06/11/1849",
+    "Location":"London",
+    "Country":"Great Britain",
+    "Info":"Charles Bray and Eliot joined Cara in London, leaving the Midlands five days after Eliot's father's funeral for a Continental tour that they had consiered taking together for some time."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 1.166,51.081 ]
+    },
+    "properties": {
+    "Date":"06/12/1849",
+    "Location":"Folkestone",
+    "Country":"Great Britain",
+    "Info":"Eliot, accompanying Charles and Cara Bray, began her first Continental Tour in Folkestone on the coast near Dover."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 2.35222,48.85661 ]
+    },
+    "properties": {
+    "Date":"06/13/1849",
+    "Location":"Paris",
+    "Country":"France",
+    "Info":"Eliot and the Brays spent two days in Paris. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 3.9742,47.8556 ]
+    },
+    "properties": {
+    "Date":"06/15/1849",
+    "Location":"Tonnerre",
+    "Country":"France",
+    "Info":"They stopped in this medieval town on the way to Southern France. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 4.83565,45.76404 ]
+    },
+    "properties": {
+    "Date":"06/1849",
+    "Location":"Lyon",
+    "Country":"France",
+    "Info":"From Lyon, they traveled down the Rhone to Avignon."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 4.80552,43.94931 ]
+    },
+    "properties": {
+    "Date":"06/1849",
+    "Location":"Avignon",
+    "Country":"France",
+    "Info":"They spent two days in Avignon."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 5.36977,43.29648 ]
+    },
+    "properties": {
+    "Date":"06/1849",
+    "Location":"Marseilles ",
+    "Country":"France",
+    "Info":"They passed through Marseilles to travel east along the coast toward Italy. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.26195,43.71017 ]
+    },
+    "properties": {
+    "Date":"06/1849",
+    "Location":"Nice ",
+    "Country":"France",
+    "Info":"From Nice, they traveled via the Corniche (along the French coastline) into Italy."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.94625,44.40564 ]
+    },
+    "properties": {
+    "Date":"06/1849",
+    "Location":"Genoa ",
+    "Country":"Italy",
+    "Info":"They spent a week in Genoa. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 9.19,45.4642 ]
+    },
+    "properties": {
+    "Date":"07/1849",
+    "Location":"Milan",
+    "Country":"Italy",
+    "Info":"They passed through on the way to the Northern Italian lakes "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 9.25716,46.01604 ]
+    },
+    "properties": {
+    "Date":"07/1849",
+    "Location":"Lake Como",
+    "Country":"Italy",
+    "Info":"They spent several days on the Northern Italian lakes. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 8.65325,45.96762 ]
+    },
+    "properties": {
+    "Date":"07/1849",
+    "Location":"Lake Maggiore ",
+    "Country":"Italy",
+    "Info":"They spent several days on the Northern Italian lakes. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 7.066667,46.1 ]
+    },
+    "properties": {
+    "Date":"07/1849",
+    "Location":"Martigny",
+    "Country":"Switzerland",
+    "Info":"They returned from Italy to Switzerland via Simplon Pass."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 6.8697,45.9231 ]
+    },
+    "properties": {
+    "Date":"07/1849",
+    "Location":"Chamonix ",
+    "Country":"Switzerland",
+    "Info":"They continued via the Col de Balme to Chamonix on the North side of the Mont Blanc on horseback, with the ultimate goal of Geneva."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 6.14315,46.20439 ]
+    },
+    "properties": {
+    "Date":"07/1849",
+    "Location":"Geneva ",
+    "Country":"Switzerland",
+    "Info":"They arrives in Geneva four days after their departure from Chamonix. After a week on the Northern shore of the lake, they returned here, and the Brays took leave of their friend on July 25 to return home to Coventry. Eliot stayed behind in a pension, Campagne Plongeon, and then moved into lodgings with the painter D'Albert Durade and his wife on October 9. She stayed all winter; on March 18, 1850, she began her return trip to England accompanied by M. D'Albert Durade. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 6.626764,46.507072 ]
+    },
+    "properties": {
+    "Date":"07/1849",
+    "Location":"Ouchy",
+    "Country":"Switzerland",
+    "Info":"Eliot and the Brays spent a week together on the north shore of Lake Geneva (Ouchy and Vevey)"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 6.85,46.466667 ]
+    },
+    "properties": {
+    "Date":"07/1849",
+    "Location":"Vevey",
+    "Country":"Switzerland",
+    "Info":"Eliot and the Brays spent a week together on the north shore of Lake Geneva (Ouchy and Vevey)"
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 1.85868,50.95129 ]
+    },
+    "properties": {
+    "Date":"03/1850",
+    "Location":"Calais",
+    "Country":"France",
+    "Info":"George Eliot and M. D'Albert Durade crossed the English Channel from Calais on their way to England."
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ 1.3089,51.1295 ]
+    },
+    "properties": {
+    "Date":"03/1850",
+    "Location":"Dover",
+    "Country":"Great Britain",
+    "Info":"The crossing to Dover was rough and made Eliot quite seasick. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -0.12775,51.50735 ]
+    },
+    "properties": {
+    "Date":"03/1850",
+    "Location":"London",
+    "Country":"Great Britain",
+    "Info":"Charles Bray and Eliot joined Cara in London, leaving the Midlands five days after Eliot's father's funeral for a Continental tour that they had consiered taking together for some time.<br>In late March, Eliot arrived in London with M. D'Albert Durade. "
+    }
+  },
+  {
+    "type": "Feature",
+    "geometry": {
+       "type": "Point",
+       "coordinates":  [ -1.510556,52.408056 ]
+    },
+    "properties": {
+    "Date":"03/25/1850",
+    "Location":"Rosehill, Coventry",
+    "Country":"Great Britain",
+    "Info":"Eliot left D'Albert in London for more sightseeing, and joined the Brays in Rosehill, their home on Radford Road near the city center."
+    }
+  }
+]
+};
