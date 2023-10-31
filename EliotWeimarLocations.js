79fe95d85500fd8740edb2ceb5bf54e633b3fd48
Wesley Lowman <72044648+wwl0007@users.noreply.github.com>
2023-10-31 03:35:48 -0500

    Update EliotWeimarLocations.js

--- EliotWeimarLocations.js
+++ EliotWeimarLocations.js
@@ -10,8 +10,7 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"06/12/1849",
     "Location":"Folkestone",
-    "Country":"Great Britain",
-    "Info":"<iframe width="560" height="315" src="https://www.youtube.com/embed/c4j9Z9bwBBU?si=_p33NhnWSYIVT4mB" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>"
+    "Info":"Hey"
     }
   },
   {
@@ -23,7 +22,6 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"06/13/1849",
     "Location":"Paris",
-    "Country":"France",
     "Info":"Eliot and the Brays spent two days in Paris. "
     }
   },
@@ -36,7 +34,6 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"06/15/1849",
     "Location":"Tonnerre",
-    "Country":"France",
     "Info":"They stopped in this medieval town on the way to Southern France. "
     }
   },
@@ -49,7 +46,6 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"06/1849",
     "Location":"Lyon",
-    "Country":"France",
     "Info":"From Lyon, they traveled down the Rhone to Avignon."
     }
   },
@@ -62,7 +58,6 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"06/1849",
     "Location":"Avignon",
-    "Country":"France",
     "Info":"They spent two days in Avignon."
     }
   },
@@ -75,7 +70,6 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"06/1849",
     "Location":"Marseilles ",
-    "Country":"France",
     "Info":"They passed through Marseilles to travel east along the coast toward Italy. "
     }
   },
@@ -88,7 +82,6 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"06/1849",
     "Location":"Nice ",
-    "Country":"France",
     "Info":"From Nice, they traveled via the Corniche (along the French coastline) into Italy."
     }
   },
@@ -101,7 +94,6 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"06/1849",
     "Location":"Genoa ",
-    "Country":"Italy",
     "Info":"They spent a week in Genoa. "
     }
   },
@@ -114,7 +106,6 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"07/1849",
     "Location":"Milan",
-    "Country":"Italy",
     "Info":"They passed through on the way to the Northern Italian lakes "
     }
   },
@@ -127,7 +118,6 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"07/1849",
     "Location":"Lake Como",
-    "Country":"Italy",
     "Info":"They spent several days on the Northern Italian lakes. "
     }
   },
@@ -140,7 +130,6 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"07/1849",
     "Location":"Lake Maggiore ",
-    "Country":"Italy",
     "Info":"They spent several days on the Northern Italian lakes. "
     }
   },
@@ -153,7 +142,6 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"07/1849",
     "Location":"Martigny",
-    "Country":"Switzerland",
     "Info":"They returned from Italy to Switzerland via Simplon Pass."
     }
   },
@@ -166,7 +154,6 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"07/1849",
     "Location":"Chamonix ",
-    "Country":"Switzerland",
     "Info":"They continued via the Col de Balme to Chamonix on the North side of the Mont Blanc on horseback, with the ultimate goal of Geneva."
     }
   },
@@ -179,7 +166,6 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"07/1849",
     "Location":"Geneva ",
-    "Country":"Switzerland",
     "Info":"They arrives in Geneva four days after their departure from Chamonix. After a week on the Northern shore of the lake, they returned here, and the Brays took leave of their friend on July 25 to return home to Coventry. Eliot stayed behind in a pension, Campagne Plongeon, and then moved into lodgings with the painter D'Albert Durade and his wife on October 9. She stayed all winter; on March 18, 1850, she began her return trip to England accompanied by M. D'Albert Durade. "
     }
   },
@@ -192,7 +178,6 @@ var EliotWeimarLocations = {
     "properties": {
     "Date":"03/25/1850",
     "Location":"Rosehill, Coventry",
-    "Country":"Great Britain",
     "Info":"Eliot left D'Albert in London for more sightseeing, and joined the Brays in Rosehill, their home on Radford Road near the city center."
     }
   }
